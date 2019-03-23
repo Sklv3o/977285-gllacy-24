e8dcb4f1810cb28d4c9df5f054f354915827fe23
sermexp <sermexp@gmail.com>
2019-03-23 23:27:06 +0300

    добавил js

--- js/write-us.js
+++ js/write-us.js
@@ -1,13 +1,15 @@
 var link = document.querySelector(".adress-btn");
+
 var popup = document.querySelector(".write-us-popup");
+
 var close = popup.querySelector(".close-btn");
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-  });
+});
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-  });
+});
