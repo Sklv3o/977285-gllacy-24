6814cc037428c3a0330bcab2d7533913bea1f7fb
sermexp <sermexp@gmail.com>
2019-03-23 12:28:42 +0300

    Правки

---
+++ js/write-us.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".adress-btn");
+var popup = document.querySelector(".write-us-popup");
+var close = popup.querySelector(".close-btn");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+  });
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
